23f8954c2daf5a0ad8dd522b6aed46ccff217efa
ahmedkaram <akaram920@gmail.com>
2021-12-08 00:05:29 +0200

    cors

--- src/main/java/com/example/authentication/AuthenticationApplication.java
+++ src/main/java/com/example/authentication/AuthenticationApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
@@ -24,4 +27,22 @@ public class AuthenticationApplication {
     public RestTemplate restTemplate(List<HttpMessageConverter<?>> messageConverters) {
         return new RestTemplate(messageConverters);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/getToken").allowedOrigins("*");
+                registry.addMapping("/isTokenValid/{token}").allowedOrigins("*");
+
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**");
+            }
+        };
+    }
+
+
 }
